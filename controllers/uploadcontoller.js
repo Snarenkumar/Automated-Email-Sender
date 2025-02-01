2cfe03af30844b7a5b3fe867c91ecfcb981da76f
S Naren Kumar <vssnaren@gmail.com>
2025-02-01 23:12:48 +0530

    working with design of the second interface

--- controllers/uploadcontoller.js
+++ controllers/uploadcontoller.js
@@ -15,7 +15,9 @@ exports.uploadExcel = (req, res) => {
         const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
         const sheetName = workbook.SheetNames[0];
         const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-
+        sheetData.forEach(element => {
+           console.log(element);
+        });
         const emails = sheetData.map(row => row.Email);
 
         setTimeout(() => {
