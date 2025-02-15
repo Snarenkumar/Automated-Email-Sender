6bc63e3458a7f0f72b1c4a09c5443fa90e6ade04
S Naren Kumar <vssnaren@gmail.com>
2025-02-15 22:19:49 +0530

    adding commemts for readability 0

--- controllers/uploadcontoller.js
+++ controllers/uploadcontoller.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const xlsx = require('xlsx');
 const transporter = require('../config/mailConfig');
-// !! here  is  in wirods 
+// !! here  is  in  
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
